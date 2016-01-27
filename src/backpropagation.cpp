0f97ab6d71c6f9e39f5525129f70a3ecf0fb73df
olanleed <olanleed@gmail.com>
2016-01-28 00:38:39 +0900

    Dropoutを除去

--- src/backpropagation.cpp
+++ src/backpropagation.cpp
@@ -3,10 +3,8 @@
 
 BackPropagation::BackPropagation(const int input_layer, const int hidden_layer,
 				 const int output_layer, const double learn_rate,
-				 const double momentum, const double weight_decay, const double max_epoch)
-  : learn_rate_(learn_rate), momentum_(momentum),
-    weight_decay_(weight_decay), max_epoch_(max_epoch),
-    hidden_(hidden_layer), masked_hidden_(hidden_layer) {
+				 const double epoch)
+  : learn_rate_(learn_rate), epoch_(epoch), hidden_(hidden_layer) {
 
   weight_input_.resize(input_layer, hidden_layer);
   weight_hidden_.resize(hidden_layer, output_layer);
@@ -24,14 +22,11 @@ BackPropagation::BackPropagation(const int input_layer, const int hidden_layer,
 
 BackPropagation::~BackPropagation(void) { }
 
-void BackPropagation::train(const std::vector< std::pair< dvector, dvector > >& data_set) {
-  for (std::size_t i = 0; i < max_epoch_; ++i) {
-    //SGD
-    for (std::size_t j = 0; j < 100; ++j) {
-      const int index = Random::generate_int(0, data_set.size() - 1);
-      const dvector mask = generate_dropout_mask(hidden_.size());
-      const dvector output = forward_propagete(data_set[index].second, mask);
-      back_propagate(data_set[index].first, data_set[index].second, output, mask);
+void BackPropagation::fit(const std::vector< std::pair< dvector, dvector > >& dataset) {
+  for (std::size_t i = 0; i < epoch_; ++i) {
+    for (const auto& data : dataset) {
+      const dvector output = forward(data.second);
+      backward(data.first, data.second, output);
       update_weight();
     }
   }
@@ -43,46 +38,38 @@ BackPropagation::dvector BackPropagation::predict(const dvector& input) {
   std::transform(hidden.begin(), hidden.end(), hidden_.begin(),
 		 [](const double x) { return 1.0 / (1.0 + std::exp(-x)); } );
 
-  dvector tmp(hidden_.size());
-  std::copy(hidden_.begin(), hidden_.end(), tmp.begin());
-  for(double& x : tmp) {
-    x /= 2.0;
-  }
-
-  return prod(tmp, weight_hidden_);
+  return prod(hidden_, weight_hidden_);
 }
 
-BackPropagation::dvector BackPropagation::forward_propagete(const dvector& input, const dvector& mask) {
+BackPropagation::dvector BackPropagation::forward(const dvector& input) {
   const dvector hidden = prod(input, weight_input_);
 
   std::transform(hidden.begin(), hidden.end(), hidden_.begin(),
 		 [](const double x) { return 1.0 / (1.0 + std::exp(-x)); } );
 
-  for (std::size_t i = 0; i < hidden_.size(); ++i) {
-    masked_hidden_[i] = hidden_[i] * mask[i];
-  }
-
-  return prod(masked_hidden_, weight_hidden_);
+  return prod(hidden_, weight_hidden_);
 }
 
-void BackPropagation::back_propagate(const dvector& answer, const dvector& input,
-				     const dvector& output, const dvector& mask) {
+void BackPropagation::backward(const dvector& answer, const dvector& input, const dvector& output) {
   const dvector delta = output - answer;
-  dvector error_hidden = prod(weight_hidden_, delta);
+  auto sigmoid = [](const double x) { return 1.0 / (1.0 + std::exp(-x)); };
+  auto diff_sigmoid = [&](const double x) { return sigmoid(x) * (1.0 - sigmoid(x)); };
 
-  for (std::size_t i = 0; i < error_hidden.size(); ++i) {
-    error_hidden[i] *= masked_hidden_[i] * (1.0 - masked_hidden_[i]);
+  dvector hidden = prod(weight_hidden_, delta);
+
+  for (std::size_t i = 0; i < hidden.size(); ++i) {
+    hidden[i] *= diff_sigmoid(hidden_[i]);
   }
 
   for (std::size_t i = 0; i < diff_weight_hidden_.size1(); ++i) {
     for (std::size_t j = 0; j < diff_weight_hidden_.size2(); ++j) {
-      diff_weight_hidden_(i, j) = masked_hidden_[i] * delta[j];
+      diff_weight_hidden_(i, j) = hidden_[i] * delta[j];
     }
   }
 
   for (std::size_t i = 0; i < diff_weight_input_.size1(); ++i) {
     for (std::size_t j = 0; j < diff_weight_input_.size2(); ++j) {
-      diff_weight_input_(i, j) = input[i] * error_hidden[j];
+      diff_weight_input_(i, j) = input[i] * hidden[j];
     }
   }
 }
@@ -90,20 +77,4 @@ void BackPropagation::back_propagate(const dvector& answer, const dvector& input
 void BackPropagation::update_weight(void) {
   weight_input_ -= learn_rate_ * diff_weight_input_;
   weight_hidden_ -= learn_rate_ * diff_weight_hidden_;
-}
-
-BackPropagation::dvector BackPropagation::generate_dropout_mask(const int max_size) {
-  dvector mask(max_size, 1.0);
-  std::set<int> unique_indices;
-
-  //半分のhidden layerを脱落させる
-  while(unique_indices.size() < (max_size / 2)) {
-    unique_indices.insert(Random::generate_int(0, max_size - 1));
-  }
-
-  for (const auto& index : unique_indices) {
-    mask(index) = 0.0;
-  }
-
-  return mask;
 }
--- src/backpropagation.hpp
+++ src/backpropagation.hpp
@@ -6,6 +6,7 @@
 #include <boost/numeric/ublas/io.hpp>
 #include <cstddef>
 #include <set>
+#include <functional>
 
 class BackPropagation {
 public :
@@ -14,9 +15,7 @@ public :
 
 private :
   const double learn_rate_;
-  const double momentum_;
-  const double weight_decay_;
-  const double max_epoch_;
+  const double epoch_;
 
 private:
   dmatrix weight_input_;
@@ -24,23 +23,21 @@ private:
   dmatrix diff_weight_input_;
   dmatrix diff_weight_hidden_;
   dvector hidden_;
-  dvector masked_hidden_;
+
 public :
   BackPropagation(const int input_layer, const int hidden_layer,
 		  const int output_layer, const double learn_rate,
-		  const double momentum, const double weight_decay, const double max_epoch);
+		  const double epoch);
 
   virtual ~BackPropagation(void);
 
-  void train(const std::vector< std::pair< dvector, dvector > >& data_set);
+  void fit(const std::vector< std::pair< dvector, dvector > >& data_set);
   dvector predict(const dvector& input);
 
 private :
-  dvector forward_propagete(const dvector& input, const dvector& mask);
-  void back_propagate(const dvector& answer, const dvector& input,
-		      const dvector& output, const dvector& mask);
+  dvector forward(const dvector& input);
+  void backward(const dvector& answer, const dvector& input, const dvector& output);
   void update_weight(void);
-  dvector generate_dropout_mask(const int max_size);
 };
 
 #endif //BACKPROPAGATION_INCLUDE_BACKPROPAGATION_HPP_
